0c0a5b7414b33c267fcd3fae695c1db714b77a34
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2025-01-23 20:09:02 -0500

    Safe area y arreglo de botones

--- src/pages/Gender.tsx
+++ src/pages/Gender.tsx
@@ -37,49 +37,51 @@ const Gender: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="background flex flex-col items-center justify-center min-h-screen">
-          {/* Flecha de retroceso */}
-          <div className="absolute top-4 left-4">
-            <IoArrowBack
-              className="text-black text-3xl cursor-pointer"
-              onClick={() => navigate.push("/languages", "back")}
+        <div className="safe-area">
+          <div className="background flex flex-col items-center justify-center min-h-screen">
+            {/* Flecha de retroceso */}
+            <div className="absolute top-4 left-4">
+              <IoArrowBack
+                className="text-black text-3xl cursor-pointer"
+                onClick={() => navigate.push("/languages", "back")}
+              />
+            </div>
+
+            {/* SVG de los puntos superiores */}
+            <img
+              src="./step2.svg"
+              alt="Progress dots"
+              className="dots1 absolute top-20"
             />
-          </div>
 
-          {/* SVG de los puntos superiores */}
-          <img
-            src="./step2.svg"
-            alt="Progress dots"
-            className="dots1 absolute top-20"
-          />
+            {/* Texto "I Identify as" */}
+            <div className="text-container flex items-center justify-center mt-16">
+              <p className="text-normal">{title[0]}</p>
+              <p className="text-highlight">{title[1]}</p>
+              <p className="text-normal">{title[2]}</p>
+            </div>
 
-          {/* Texto "I Identify as" */}
-          <div className="text-container flex items-center justify-center mt-16">
-            <p className="text-normal">{title[0]}</p>
-            <p className="text-highlight">{title[1]}</p>
-            <p className="text-normal">{title[2]}</p>
-          </div>
-
-          {/* Botones de opciones */}
-          <div className="buttons-container flex gap-4 mt-8">
-            <button
-              className="ionic-button "
-              onClick={async () => {
-                handleGenderChange("W")
-                await Haptics.impact({ style: ImpactStyle.Medium });
-              }}
-            >
-              {options[0]}
-            </button>
-            <button
-              className="ionic-button"
-              onClick={async () => {
-                handleGenderChange("M")
-                await Haptics.impact({ style: ImpactStyle.Medium });
-              }}
-            >
-              {options[1]}
-            </button>
+            {/* Botones de opciones */}
+            <div className="buttons-container flex gap-4 mt-8">
+              <button
+                className="gender-button"
+                onClick={async () => {
+                  handleGenderChange("W")
+                  await Haptics.impact({ style: ImpactStyle.Medium });
+                }}
+              >
+                {options[0]}
+              </button>
+              <button
+                className="gender-button"
+                onClick={async () => {
+                  handleGenderChange("M")
+                  await Haptics.impact({ style: ImpactStyle.Medium });
+                }}
+              >
+                {options[1]}
+              </button>
+            </div>
           </div>
         </div>
       </IonContent>
--- src/pages/Languages.tsx
+++ src/pages/Languages.tsx
@@ -19,44 +19,46 @@ const Languages = ({ backTo }: { backTo: string }) => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className="background flex flex-col items-center justify-center min-h-screen">
-          {/* Flecha de retroceso */}
-          <div className="absolute top-4 left-4">
-            <IoArrowBack
-              className="text-black text-3xl cursor-pointer"
-              onClick={() => navigate.push(backTo || "/", "back")}
-            />
-          </div>
+        <div className="safe-area">
+          <div className="background flex flex-col items-center justify-center min-h-screen">
+            {/* Flecha de retroceso */}
+            <div className="absolute top-4 left-4">
+              <IoArrowBack
+                className="text-black text-3xl cursor-pointer"
+                onClick={() => navigate.push(backTo || "/", "back")}
+              />
+            </div>
 
-          {/* Puntos superiores */}
-          {backTo === undefined && (
-            <img
-              src="./step1.svg"
-              alt="Progress dots"
-              className="dots1 absolute top-20"
-            />
-          )}
+            {/* Puntos superiores */}
+            {backTo === undefined && (
+              <img
+                src="./step1.svg"
+                alt="Progress dots"
+                className="dots1 absolute top-20"
+              />
+            )}
 
-          {/* Idiomas */}
-          <div className="buttons-container flex flex-col items-center gap-8 mt-16">
-            <button
-              className="language-button"
-              onClick={() => handleLanguageChange("es")}
-            >
-              Español
-            </button>
-            <button
-              className="language-button"
-              onClick={() => handleLanguageChange("en")}
-            >
-              English
-            </button>
-            <button
-              className="language-button"
-              onClick={() => handleLanguageChange("fr")}
-            >
-              Français
-            </button>
+            {/* Idiomas */}
+            <div className="buttons-container flex flex-col items-center gap-8 mt-16">
+              <button
+                className="language-button"
+                onClick={() => handleLanguageChange("es")}
+              >
+                Español
+              </button>
+              <button
+                className="language-button"
+                onClick={() => handleLanguageChange("en")}
+              >
+                English
+              </button>
+              <button
+                className="language-button"
+                onClick={() => handleLanguageChange("fr")}
+              >
+                Français
+              </button>
+            </div>
           </div>
         </div>
       </IonContent>
